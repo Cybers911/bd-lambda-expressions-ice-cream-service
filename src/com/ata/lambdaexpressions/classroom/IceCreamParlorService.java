dd55b63c91c2e88c4fb0497d320044334738200f
Cybers911 <info@cybers911.com>
2025-02-12 22:16:23 -0600

    sol1

--- src/com/ata/lambdaexpressions/classroom/IceCreamParlorService.java
+++ src/com/ata/lambdaexpressions/classroom/IceCreamParlorService.java
@@ -1,5 +1,6 @@
 package com.ata.lambdaexpressions.classroom;
 
+import com.ata.lambdaexpressions.classroom.converter.RecipeConverter;
 import com.ata.lambdaexpressions.classroom.dao.CartonDao;
 import com.ata.lambdaexpressions.classroom.dao.RecipeDao;
 import com.ata.lambdaexpressions.classroom.exception.CartonCreationFailedException;
@@ -17,6 +18,8 @@ import java.util.Queue;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
+
+import static java.util.stream.Nodes.collect;
 
 /**
  * Provides Ice Cream Parlor functionality.
@@ -52,6 +55,24 @@ public class IceCreamParlorService {
         List<Carton> cartons = cartonDao.getCartonsByFlavorNames(flavorNames);
 
         // PHASE 1: Use removeIf() to remove any empty cartons from cartons
+        /*cartons.removeIf((carton) -> carton.isEmpty());*/
+/*
+        cartons.removeIf(carton -> carton.isEmpty());
+*/
+        cartons.removeIf(Carton::isEmpty);// Method reference for removeIf()
+
+
+
+
+
+
+
+        //Pass each carton to a lambda expression to see if its empty
+
+
+
+
+
 
         return buildSundae(cartons);
     }
@@ -62,6 +83,12 @@ public class IceCreamParlorService {
 
         // PHASE 2: Use forEach() to add one scoop of each flavor
         // remaining in cartons
+
+        cartons.forEach((aCarton) -> {sundae.addScoop(aCarton.getFlavor());});
+        
+        //Go through a list oCartons we and add one scoop to the sundae choosing the flavor
+        //Since cartons is a list we can use a list forEach instead of a Stream
+
 
         return sundae;
     }
@@ -78,9 +105,11 @@ public class IceCreamParlorService {
      * @return the number of cartons produced by the ice cream maker
      */
     public int prepareFlavors(List<String> flavorNames) {
+        // this is a map operation to convert List<String> to List<Recipe>
+        // not a stream map
         List<Recipe> recipes = map(
-            flavorNames,
-            (flavorName) -> {
+            flavorNames,//we call inputin the helper method
+            (flavorName) -> {we call converter inthe helper
                 // trap the checked exception, RecipeNotFoundException, and
                 // wrap in a runtime exception because our lambda can't throw
                 // checked exceptions
@@ -93,9 +122,19 @@ public class IceCreamParlorService {
         );
 
         // PHASE 3: Replace right hand side: use map() to convert List<Recipe> to List<Queue<Ingredient>>
-        List<Queue<Ingredient>> ingredientQueues = new ArrayList<>();
+        /*List<Queue<Ingredient>> ingredientQueues = new ArrayList<>();
+        * All what we are doing in the Lambda expression is calling a static method in a class we can
+        *  use a method
+        * in the Lambda express*/
 
-        return makeIceCreamCartons(ingredientQueues);
+         /*List<Queue<Ingredient>> ingredientQueues = recipes.stream()
+                 .map(RecipeConverter::fromRecipeToIngredientQueue)
+                 .collect(Collectors.toList());*/
+
+        List<Queue<Ingredient>> ingredientQueues = recipes.stream()
+                .map(aRecipe) -> RecipeConverter.fromRecipeToIngredientQueue(aRecipe)
+                .collect(Collectors.toList());
+         return makeIceCreamCartons(ingredientQueues);
     }
 
     @VisibleForTesting
@@ -105,7 +144,10 @@ public class IceCreamParlorService {
         for (Queue<Ingredient> ingredients : ingredientQueues) {
 
             // PHASE 4: provide Supplier to prepareIceCream()
-            if (iceCreamMaker.prepareIceCreamCarton(null)) {
+            //Use a Lambda expression to get out ingredients from the Queue ingrdients
+            // and pass it to iceCreamMaker.prepareIceCreamCarton()
+            // If iceCreamMaker.prepareIceCreamCarton() returns true, increment cartonsCreated by 1.
+            if (iceCreamMaker.prepareIceCreamCarton(() -> ingredients.poll())) {
                 cartonsCreated++;
             }
         }
@@ -119,10 +161,21 @@ public class IceCreamParlorService {
      *
      * (We will get to Java streams in a later lesson, at which point we won't need a helper method
      * like this.)
+     * 
+     * @Param List
+     * @Param Functional Reference name a method or Lambda expression
+     * <T, R> means 2 generics data will be referenced
+     * List<T> means a List of the first generic data
+     * List<R> means a list of the second generic data
+     * function<T, R> means a method that have 2 parameters
+     *  FUnction converter receive two parameters 1 and 2nd types
+     *  List<R> RETURN A 2ND TYPE AND RECEIVE A List<T> of 1st type</T> 
      */
     private <T, R> List<R> map(List<T> input, Function<T, R> converter) {
+        //Use the stram interfce map() method to dun the converter method (second parameter)
+        //passed to it
         return input.stream()
-            .map(converter)
+            .map(converter)// Pass the function we received to the stram map
             .collect(Collectors.toList());
     }
 }
